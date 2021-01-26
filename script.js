e48cde96bb8d22614e09c3c7f1a45728a82132cc
Hanna <Hanna@Taylors-MacBook-Pro-2.local>
2021-01-26 12:24:24 -0600

    successfully connected the API and made a call

---
+++ script.js
@@ -0,0 +1,24 @@
+// ** 1. CONNECT API AND MAKE FIRST CALL
+// ** 2. TARGET CORRECT POKEMON
+// ** 3. ASSIGN USER INPUT 
+
+// const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=151`
+
+
+
+async function fetchOrigPokes(pokemonName) {
+  const allPokemon = `https://pokeapi.co/api/v2/pokemon/`
+
+  try {
+    const response = await axios.get(`${allPokemon}${pokemonName}`)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchOrigPokes('togepi')
+
+
+
+
