62cec658caf213243530519e7fcf4170ad86f8da
Hanna <Hanna@Taylors-MacBook-Pro-2.local>
2021-01-26 12:52:28 -0600

    added eventListener that logs search value

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+console.log('Welcome to my site! Dev + Design by Hanna Boorom')
+
+
 // ** 1. CONNECT API AND MAKE FIRST CALL
 // ** 2. TARGET CORRECT POKEMON
 // ** 3. ASSIGN USER INPUT 
@@ -5,7 +8,7 @@
 // const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=151`
 
 
-
+// CONNECTING API AND VERIFYING THAT INFO I WANT IS DISPLAYING IN CONSOLE
 async function fetchOrigPokes(pokemonName) {
   const allPokemon = `https://pokeapi.co/api/v2/pokemon/`
 
@@ -17,7 +20,19 @@ async function fetchOrigPokes(pokemonName) {
   }
 }
 
-fetchOrigPokes('pikachu')
+// fetchOrigPokes('pikachu')
+
+
+// ** Attach an event listener that simply logs the text value of the input...
+const selectInput = document.querySelector('.search-section')
+selectInput.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const inputValue = document.querySelector('#search-bar').value
+  // console.log(inputValue)
+  fetchOrigPokes(inputValue)
+})
+
+
 
 
 
